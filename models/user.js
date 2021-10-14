5eaaec0c3314d5430e6956203af9c00f68469843
yogesh <etpr.yogesh@gmail.com>
2021-10-14 21:11:47 +0530

    user model to store user data

---
+++ models/user.js
@@ -0,0 +1,29 @@
+// const mongoose = require("mongoose");
+// const { switchToCurrentChat } = require("telegraf/typings/button");
+
+// const userSchema = new mongoose.Schema(
+//   {
+//     first_name: {
+//       type: String,
+//       required: true
+//     },
+//     username: {
+//       type:String,
+//       required:switchToCurrentChat
+
+//     },
+//     tele_id: {
+//         type: String,
+//     },
+    
+//     vaccine: {
+//       type: String,
+//       enum:["no","1","2"],
+//     }
+//   },
+//   {
+//     timestamps: true,
+//   }
+
+// );
+// module.exports = mongoose.model("User", userSchema);
